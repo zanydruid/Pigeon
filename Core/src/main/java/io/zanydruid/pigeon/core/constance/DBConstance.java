2dd88a2d0c78a05a27122d8d87d0433fdea0a3d4
zanydruid <zanydruid528@gmail.com>
2017-07-05 15:14:39 -0400

    Begin to work on core module.

---
+++ Core/src/main/java/io/zanydruid/pigeon/core/constance/DBConstance.java
@@ -0,0 +1,7 @@
+package io.zanydruid.pigeon.core.constance;
+
+/**
+ * Created by yizhu on 7/5/17.
+ */
+public class DBConstance {
+}
---
+++ Core/src/main/java/io/zanydruid/pigeon/core/entity/Customer.java
@@ -0,0 +1,12 @@
+package io.zanydruid.pigeon.core.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * Created by yizhu on 7/5/17.
+ */
+@Entity
+@Table(name="Customer")
+public class Customer {
+}
