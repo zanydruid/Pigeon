6b0afeb0e24a66d636cbde98fdda2fce364a9b55
zanydruid <zanydruid528@gmail.com>
2017-07-05 19:59:06 -0400

    Customer model created.

--- Core/src/main/java/io/zanydruid/pigeon/core/entity/Customer.java
+++ Core/src/main/java/io/zanydruid/pigeon/core/entity/Customer.java
@@ -1,12 +1,34 @@
 package io.zanydruid.pigeon.core.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Data;
+
+import javax.persistence.*;
 
 /**
- * Created by yizhu on 7/5/17.
+ * Model maps to table Customer in DB.
  */
+@Data
 @Entity
 @Table(name="Customer")
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customerId")
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "address")
+    private String address;
+
+    @ManyToOne
+    @JoinColumn(name = "provinceId")
+    private int provinceId;
+
+    @Column(name = "phone")
+    private int phone;
+
+
 }
